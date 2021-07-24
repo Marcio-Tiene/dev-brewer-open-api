0208cc2bd1c1df97e9fafe51f218ade1f5cfce48
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-07-24 14:03:46 -0300

    some configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB),
+  ],
 })
 export class AppModule {}

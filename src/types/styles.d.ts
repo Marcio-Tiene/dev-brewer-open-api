c768c71fc5efba45e888dd2d3bae360be1ca7df3
Marcio Tiene <marcio.ricardo@mettzer.com>
2022-01-06 22:58:06 -0300

    type styles

---
+++ src/types/styles.d.ts
@@ -0,0 +1,23 @@
+declare type TCategory = {
+  category: TBjcp2015Categories;
+  'carb-min': number;
+  'fg-max': number;
+  'og-min': number;
+  name: string;
+  type: string;
+  'style-letter': string;
+  'abv-min': number;
+  'fg-min': number;
+  'category-number': string;
+  'carb-max': number;
+  'ibu-max': number;
+  ingredients: string;
+  examples: string;
+  notes: string;
+  'og-max': number;
+  'color-min': number;
+  'abv-max': number;
+  'color-max': number;
+  profile: string;
+  'ibu-min': number;
+};
